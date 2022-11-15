6c80212ea188ded1aef05cd25d78ebbb503f60d6
josgarmor88 <josgarmor88@gmail.com>
2022-11-15 15:06:43 +0100

    Nunit test project for WorldCupScoreBoard

---
+++ WorldCupScoreBoard/WorldCupScoreBoardTests/WCScoreBoardTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace WorldCupScoreBoardTests
+{
+    public class WCScoreBoardTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
